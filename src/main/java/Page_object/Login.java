82b1f703a67e5aaed5bf3bfee7f6c1d41f356eab
admin <akshayshetty531@gmail.com>
2024-04-13 03:50:47 +0530

    add to github

--- src/main/java/Page_object/Login.java
+++ src/main/java/Page_object/Login.java
@@ -1,4 +1,16 @@
 package Page_object;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class Login {
+    WebDriver rdriver;
+
+    public Login(WebDriver rdriver)
+
+    {
+        this.rdriver=rdriver;
+        PageFactory.initElements(rdriver,this);
+    }
+
 }
